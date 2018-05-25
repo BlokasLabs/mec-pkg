247577bd390506cb51e937a7fbdb53424e2cbf5e
Giedrius <giedrius@blokas.io>
2018-05-25 13:12:02 +0000

    MEC deb packaging scripts

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p build
+cd build
+
+export CC=gcc-4.9
+export CXX=g++-4.9
+cmake ../MEC -DDISABLE_LIBUSB=1 -DDISABLE_SOUNDPLANELITE=1 -DDISABLE_PUSH2=1 -DDISABLE_EIGENHARP=1
+
+make -j4
